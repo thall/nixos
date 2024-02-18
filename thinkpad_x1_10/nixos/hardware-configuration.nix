573f5737e0b872f391c9a0cf7cda1d69f13115d2
Niclas Thall <niclas.thall@einride.tech>
2024-02-18 14:47:03 +0100

    feat(thinkpad_x1_10): add swap file

--- thinkpad_x1_10/nixos/hardware-configuration.nix
+++ thinkpad_x1_10/nixos/hardware-configuration.nix
@@ -31,7 +31,10 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024; # 16GB
+  } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
