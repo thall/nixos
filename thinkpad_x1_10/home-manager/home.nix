5918604406167cd6ea9e4578406f9f1bedf09741
Niclas Thall <niclas.thall@einride.tech>
2024-02-18 14:07:32 +0100

    feat(thinkpad_x1_10): add initial configuration

---
+++ thinkpad_x1_10/home-manager/home.nix
@@ -0,0 +1,268 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.binutils # readelf
+    pkgs.colordiff
+    pkgs.curl
+    pkgs.gcc # Needed to compile / run Go programs
+    pkgs.gnumake
+    pkgs.google-cloud-sdk
+    pkgs.lsof
+    pkgs.htop
+    pkgs.hypnotix
+    pkgs.hwinfo
+    pkgs.jetbrains.goland
+    pkgs.jq
+    pkgs.ledger-live-desktop
+    pkgs.monero-cli
+    pkgs.monero-gui
+    pkgs.nixpkgs-fmt
+    pkgs.nodejs-18_x
+    pkgs.pciutils
+    pkgs.peek # tool for recording GIFs
+    pkgs.python3
+    pkgs.ripgrep
+    pkgs.signal-desktop
+    pkgs.spotify
+    pkgs.terraform
+    pkgs.tree
+    pkgs.unzip
+    pkgs.usbutils
+    pkgs.vlc
+    pkgs.vscode
+    pkgs.wget
+    pkgs.wl-clipboard
+    pkgs.xclip
+    pkgs.yarn
+    pkgs.yq
+    pkgs.qgis
+  ];
+
+  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        shell.program = "${pkgs.tmux}/bin/tmux";
+        window = {
+          padding = {
+            x = 4;
+            y = 0;
+          };
+          dynamic_padding = true;
+          decorations = "full";
+          startup_mode = "Maximized";
+          title = "terminal";
+          dynamic_title = true;
+        };
+
+        font = {
+          normal = {
+            family = "Hack";
+          };
+          size = 10.0;
+        };
+
+        selection = {
+          save_to_clipboard = true;
+        };
+
+
+        live_config_reload = true;
+      };
+    };
+
+    bash = {
+      enable = true;
+      historyIgnore = [ "ls" "cd" "exit" ];
+      shellAliases = {
+        gapit = "gcloud auth print-identity-token";
+        g = "git";
+        gcaan = "git commit -a --amend --no-edit";
+        gchma = "git checkout master && git fetch && git reset --hard origin/master";
+        grexec = "git rebase --exec='cd backend && make' origin/master";
+        gau = "git add -u";
+        gd = "git diff";
+        gdc = "git diff --cached";
+        gf = "git fetch";
+        gl = "git log";
+        gp = "git pull";
+        gsh = "git show";
+        gs = "git status";
+        gri = "git rebase -i HEAD~10";
+        grc = "git rebase --continue";
+        grh = "git reset --hard";
+        gpfl = "git push --force-with-lease";
+        gpuh = "git push";
+        hme = "home-manager edit";
+        hms = "home-manager switch";
+        urldec = "python -c \"import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))\"";
+        urlenc = "python -c \"import sys, urllib.parse as ul; print (ul.quote_plus(sys.argv[1]))\"";
+        xcp = "xclip -selection c";
+      };
+      # workaround to get the EDITOR env set.
+      # Assigning `sessionVariables` does not work.
+      # LESSQUIET, see https://github.com/wofr06/lesspipe/issues/103
+      initExtra = ''
+        export EDITOR="vim"
+        export LESSQUIET="true"
+        # source <(bookctl completion bash)
+        # source <(deliverctl completion bash)
+        # source <(orchestratectl completion bash)
+        # source <(ownctl completion bash)
+        # source <(sagaiamctl completion bash)
+        # source <(collector completion bash)
+      '';
+    };
+
+    git = {
+      enable = true;
+      userName = "Niclas Thall";
+      userEmail = "niclas.thall@einride.tech";
+      aliases = {
+        st = "status";
+      };
+      extraConfig = {
+        pull = {
+          rebase = "true";
+        };
+        commit = {
+          verbose = "true";
+          gpgSign = "true";
+        };
+        gpg = {
+          format = "ssh";
+        };
+        user = {
+          signingKey = "${config.home.homeDirectory}/.ssh/id_ed25519";
+        };
+      };
+    };
+
+    gh = {
+      enable = true;
+    };
+
+    go = {
+      enable = true;
+      package = pkgs.go_1_21;
+      goPrivate = [ "github.com/einride" "go.einride.tech" ];
+      goPath = "go";
+      goBin = "go/bin";
+    };
+
+    readline = {
+      enable = true;
+      variables = {
+        completion-ignore-case = "on";
+      };
+    };
+
+    java = {
+      enable = true;
+      package = pkgs.openjdk17;
+    };
+
+    firefox = {
+      enable = true;
+    };
+
+    tmux = {
+      enable = true;
+      clock24 = true;
+      historyLimit = 5000;
+      keyMode = "vi";
+      terminal = "screen-256color";
+      extraConfig = ''
+        # Colors
+        set -g default-terminal "tmux-256color" 
+        set -ga terminal-overrides ",xterm-termite:Tc"
+
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi y send-keys -X copy-selection
+        bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
+        bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
+
+        # Open new tab / window in CWD
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+
+        # Windows
+        set -g xterm-keys on
+        set -g base-index 1
+
+        # Panes
+        bind-key -n S-Left select-pane -L
+        bind-key -n S-Right select-pane -R
+        bind-key -n S-Up select-pane -U
+        bind-key -n S-Down select-pane -D
+      '';
+    };
+
+    vim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-airline
+      ];
+      extraConfig = ''
+        syntax on
+        set t_Co=256
+      '';
+    };
+  };
+
+  # Add Go bin directory to $PATH
+  # Add local bin directory to $PATH
+  home.sessionPath = [ "~/go/bin" "~/.local/bin" ];
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "thall";
+  home.homeDirectory = "/home/thall";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/thall/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ thinkpad_x1_10/nixos/configuration.nix
@@ -0,0 +1,153 @@
+# Edit this configuration file to define what should be installed on
+
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "sv-latin1";
+  };
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "se";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable xdg portal to get screen sharing to work on wayland
+  xdg.portal = {
+    enable = true;
+  };
+
+  # Support running non-nix binaries.
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
+  # Enable sound with PipeWire
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    # alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.fwupd.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
+  users.users.thall = {
+    createHome = true;
+    isNormalUser = true;
+    extraGroups = [ "podman" "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
+    hashedPassword = "$6$RG2hGR1grUT6Li$vDRayal/o2YW7HJ3yj0s8JjI/IgUGTJpGY8oo56IerVige8fBEvWv4VTJ3eV64WQ0cYoUSxzkZs0ijZ40J5sM1";
+  };
+
+  # For Google Chrome
+  nixpkgs.config.allowUnfree = true;
+
+  # Start ssh agent
+  programs.ssh.startAgent = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+ 
+  virtualisation = {
+    docker.enable = false;
+    podman = {
+      enable = true;
+      # alias docker = podman
+      dockerCompat = true;
+      # KO build require a docker daemon to be available.
+      # See https://github.com/ko-build/ko/issues/771
+      dockerSocket.enable = true;
+    };
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Ledger udev rules
+  # Required for Ledger Live to detect Ledger Nano S Plus via USB
+  # https://github.com/LedgerHQ/udev-rules/blob/2776324af6df36c2af4d2e8e92a1c98c281117c9/20-hw1.rules
+  # Assigns `thall` as owner.
+  services.udev.extraRules = ''
+    # Ledger Test, Nano S Plus (idProduct=5011)
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0005|5000|5001|5002|5003|5004|5005|5006|5007|5008|5009|500a|500b|500c|500d|500e|500f|5010|5011|5012|5013|5014|5015|5016|5017|5018|5019|501a|501b|501c|501d|501e|501f", TAG+="uaccess", TAG+="udev-acl", OWNER="thall"
+  '';
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
