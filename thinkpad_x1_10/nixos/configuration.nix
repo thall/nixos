b07513bba74c61d488f8baf4087475e1863307bc
Niclas Thall <niclas.thall@einride.tech>
2024-04-19 13:54:43 +0200

    feat(thinkad_x1_10): disable openssh server

--- thinkpad_x1_10/nixos/configuration.nix
+++ thinkpad_x1_10/nixos/configuration.nix
@@ -116,7 +116,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh.enable = false;
 
   # Ledger udev rules
   # Required for Ledger Live to detect Ledger Nano S Plus via USB
