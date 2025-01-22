f8406bcc9c9fb55a51ff4c03f375dbdba0226091
Niclas Thall <niclas.thall@einride.tech>
2025-01-22 20:07:59 +0100

    feat: init flakes WIP

--- thinkpad_x1_10/nixos/configuration.nix
+++ thinkpad_x1_10/nixos/configuration.nix
@@ -3,7 +3,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
