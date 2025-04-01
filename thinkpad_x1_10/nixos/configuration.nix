14bc0ef44848717c4ab0c6940587e42fb54632db
Niclas Thall <niclas.thall@einride.tech>
2025-04-01 20:53:24 +0200

    feat(thinkpad_x1_10): init flake and change hostname

--- thinkpad_x1_10/nixos/configuration.nix
+++ thinkpad_x1_10/nixos/configuration.nix
@@ -3,7 +3,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
@@ -19,7 +19,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "x1-10"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
