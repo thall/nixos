605f0abc08e74aca5ad472ea4eaa0dd025e0ad48
Niclas Thall <niclas.thall@einride.tech>
2025-11-09 09:40:04 +0100

    feat(x1_10/nixos): cleanup

--- thinkpad_x1_10/nixos/configuration.nix
+++ thinkpad_x1_10/nixos/configuration.nix
@@ -54,10 +54,6 @@
   };
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "se";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
   # Enable xdg portal to get screen sharing to work on wayland
   xdg.portal = {
     enable = true;
@@ -118,21 +114,6 @@
     pagodenVPN = { config = '' config /home/thall/einride/niclas.thall.openvpn.conf ''; };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
- 
   virtualisation = {
     docker = {
       enable = true;
@@ -151,8 +132,6 @@
     virt-manager
   ];
   services.qemuGuest.enable = true;
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = false;
